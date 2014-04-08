60760dd498533db72839237501a2a3b5d11aa57e
MR-SLW <tianshan12531@163.com>
2014-04-08 11:05:47 +0800

    edit

---
+++ script/jquery-func.js
@@ -0,0 +1,27 @@
+$( document ).ready( function(){
+	$('.portfolio-holder ul').jcarousel({
+		scroll: 1,
+		wrap: 'both',
+		initCallback: _carousel_init,
+		buttonNextHTML: null,
+		buttonPrevHTML: null
+	});
+
+	$('#navigation').hover(
+		function(){ $(this).animate({ top: '0px' }, 1300);  },
+		function(){ $(this).animate({ top: '-250px' }, 900 ); }
+	);
+	
+});
+
+function _carousel_init(carousel) {
+	$('.portfolio .portfolio-nav .next').bind('click', function() {
+		carousel.next();
+		return false;
+	});
+
+	$('.portfolio .portfolio-nav .prev').bind('click', function() {
+		carousel.prev();
+		return false;
+	});
+};
